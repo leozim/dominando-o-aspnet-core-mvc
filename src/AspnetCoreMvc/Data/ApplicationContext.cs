1b4768135bf76ded5ac1ac2e063111c03d290af1
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-01-23 11:36:01 -0300

    preparando o projeto

---
+++ src/AspnetCoreMvc/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using AspnetCoreMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspnetCoreMvc.Data;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Produto> Produtos { get; set; }
+    
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+        
+    }
+}
---
+++ src/AspnetCoreMvc/Models/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace AspnetCoreMvc.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Imagem { get; set; }
+    public string? Valor { get; set; }
+}
