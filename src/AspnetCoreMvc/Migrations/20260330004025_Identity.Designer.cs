3f36be6ac53759b6e978e81e16f28e30602028df
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-03-29 21:41:41 -0300

    criando migration do Identity e update no BD

--- src/AspnetCoreMvc/Migrations/20260330004025_Identity.Designer.cs
+++ src/AspnetCoreMvc/Migrations/20260330004025_Identity.Designer.cs
@@ -4,6 +4,7 @@ using AspnetCoreMvc.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AspnetCoreMvc.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20260330004025_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
