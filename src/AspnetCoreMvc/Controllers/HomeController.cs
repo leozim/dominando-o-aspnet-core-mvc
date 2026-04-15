44d7dbeb10ae666994bbc2bd6f86e427c4cced50
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-04-15 15:55:51 -0300

    configurando ambientes de execução

--- src/AspnetCoreMvc/Controllers/HomeController.cs
+++ src/AspnetCoreMvc/Controllers/HomeController.cs
@@ -15,6 +15,8 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRIONMENT");
+        
         return View();
     }
 
