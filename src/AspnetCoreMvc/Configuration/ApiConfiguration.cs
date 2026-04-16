e313a8e043d2bed760ad945464e3d02c165b6e90
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-04-16 18:06:07 -0300

    preparando arquivos de configuracoes de acesso ao dominio

---
+++ src/AspnetCoreMvc/Configuration/ApiConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace AspnetCoreMvc.Configuration;
+
+public class ApiConfiguration
+{
+    public const string ConfigName = "ApiConfiguration";
+
+    public string? Domain { get; set; }
+    public string? UserKey { get; set; }
+    public string? UserSecret { get; set; }
+}
