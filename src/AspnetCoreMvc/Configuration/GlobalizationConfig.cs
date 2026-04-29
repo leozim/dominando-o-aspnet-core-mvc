34a6ed9f445ba0a7d4add37b02b1960a0be60fec
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-04-29 15:32:37 -0300

    trabalhando com globalization

--- src/AspnetCoreMvc/Configuration/GlobalizationConfig.cs
+++ src/AspnetCoreMvc/Configuration/GlobalizationConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace AspnetCoreMvc.Configuration;
 
@@ -7,6 +8,15 @@ public static class GlobalizationConfig
     public static WebApplication UseGlobalizationConfig(this WebApplication app)
     {
         var defaultCulture = new CultureInfo("pt-BR");
+
+        var localizationOptions = new RequestLocalizationOptions
+        {
+            DefaultRequestCulture = new RequestCulture(defaultCulture),
+            SupportedCultures = new List<CultureInfo> { defaultCulture },
+            SupportedUICultures = new List<CultureInfo> { defaultCulture }
+        };
+        
+        app.UseRequestLocalization(localizationOptions);
         
         return app;
     }
