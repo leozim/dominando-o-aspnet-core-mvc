06178a8b5a5a8b415a56e4e1c095dc1ee5dcc8e1
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-04-29 12:41:24 -0300

    configurando Culture para Globalization

---
+++ src/AspnetCoreMvc/Configuration/GlobalizationConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace AspnetCoreMvc.Configuration;
+
+public static class GlobalizationConfig
+{
+    public static WebApplication UseGlobalizationConfig(this WebApplication app)
+    {
+        var defaultCulture = new CultureInfo("pt-BR");
+        
+        return app;
+    }
+}
