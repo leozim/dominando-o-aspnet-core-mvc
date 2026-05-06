cf5736f4621a64a4404d89a6f2d5f139611036c0
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-05-05 21:40:26 -0300

    ajustando validações front-end

--- src/AspnetCoreMvc/Models/Produto.cs
+++ src/AspnetCoreMvc/Models/Produto.cs
@@ -1,9 +1,15 @@
-﻿namespace AspnetCoreMvc.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspnetCoreMvc.Models;
 
 public class Produto
 {
+    [Key]
     public int Id { get; set; }
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
     public string? Nome { get; set; }
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
     public string? Imagem { get; set; }
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
     public string? Valor { get; set; }
 }
