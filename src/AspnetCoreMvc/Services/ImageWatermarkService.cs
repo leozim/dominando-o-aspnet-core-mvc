a73d7c4409e53720db1785f68ce53329fd9df014
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-05-27 09:15:34 -0300

    adicionando marca dagua nas imagens

---
+++ src/AspnetCoreMvc/Services/ImageWatermarkService.cs
@@ -0,0 +1,9 @@
+﻿namespace AspnetCoreMvc.Services;
+
+public class ImageWatermarkService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
